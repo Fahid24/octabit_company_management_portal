e6ca25f907d6f811f394beb41a714552ace50c67
Md. Parvez Ahmed <mdparvez.jhe@gmail.com>
2026-01-03 09:54:41 +0600

    new url added

--- src/constant/baseURL.js
+++ src/constant/baseURL.js
@@ -2,9 +2,9 @@ export const production = false;
 export const development = true;
 export const staging = false;
 export const baseURL = production
-  ? "https://8fp9fvw1-5001.inc1.devtunnels.ms/"
+  ? "https://4njg1pst-5001.inc1.devtunnels.ms/"
   : development
-  ? "https://8fp9fvw1-5001.inc1.devtunnels.ms/"
+  ? "https://4njg1pst-5001.inc1.devtunnels.ms/"
   : staging
   ? "https://monkeyman-backend.vercel.app/"
   : "https://hpjql7hm-5000.usw3.devtunnels.ms/";
